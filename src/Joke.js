d15b17b88c08eaab90e3d2ae407acb7d3ec50ee3
Apoorv Tripathi <46025704+apoorvtripathi1999@users.noreply.github.com>
2022-12-22 15:40:48 +0530

    Update Joke.js

--- src/Joke.js
+++ src/Joke.js
@@ -6,7 +6,7 @@ function Joke(props) {
 
   return (
     <div className="card my-4">
-      <h3>{props.jokeData.length>0?props.jokeData: "Appo don"}</h3>
+      <h3>{props.jokeData.length>0?props.jokeData: "Click this button to get an awsome joke Parkour!!"}</h3>
     </div>
   );
 }
